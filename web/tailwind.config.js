236ccabb8cfe76cc1d9e52f5ec67865b73191306
PedroHenriqueMG <pedrohdefsouza@gmail.com>
2024-04-23 12:26:10 -0300

    add: create folder structure

--- web/tailwind.config.js
+++ web/tailwind.config.js
@@ -1,11 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./pages/**/*.{ts,tsx}",
-    "./components/**/*.{ts,tsx}",
-    "./app/**/*.{ts,tsx}",
-    "./src/**/*.{ts,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {},
   },

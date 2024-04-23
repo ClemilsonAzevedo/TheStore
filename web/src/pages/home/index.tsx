bb50e0ff75cdac266db5191c2b44c82b9685f370
PedroHenriqueMG <pedrohdefsouza@gmail.com>
2024-04-23 12:53:16 -0300

    start: config shadcn

--- web/src/pages/home/index.tsx
+++ web/src/pages/home/index.tsx
@@ -1,7 +1,10 @@
+import { Button } from "../../components/ui/button";
+
 export default function Home() {
   return (
     <div>
       <p className="text-3xl font-bold underline">Clema</p>
+      <Button>Teste</Button>
     </div>
   );
 }
--- web/src/routes/routes.tsx
+++ web/src/routes/routes.tsx
@@ -1,5 +1,5 @@
 import { Route, Routes as RoutesDOM } from "react-router-dom";
-import Home from "../pages/home/home";
+import Home from "../pages/home";
 import LayoutDefault from "../layouts/layoutDefault";
 
 export default function Routes() {

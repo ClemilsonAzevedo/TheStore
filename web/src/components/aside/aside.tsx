e73a4799ce205adb40a1fe3e302cc3a0e77d2e6e
PedroHenriqueMG <pedrohdefsouza@gmail.com>
2024-04-24 16:29:14 -0300

    style: put hover on aside icons

--- web/src/components/aside/aside.tsx
+++ web/src/components/aside/aside.tsx
@@ -11,16 +11,29 @@ export default function Aside() {
   return (
     <section className="flex h-[550px] w-[50px] flex-col items-center justify-between">
       <div>
-        <ShoppingCart />
+        <div className="bg-white relative rounded-full p-2 duration-300 hover:bg-neutral-600">
+          <div className="absolute right-1 top-2 h-[5px] w-[5px] rounded-full bg-violet-500"></div>
+          <ShoppingCart />
+        </div>
       </div>
       <div className="flex flex-col gap-5">
-        <Home />
-        <Heart />
-        <ShoppingBasket />
+        <div className="bg-white rounded-full p-2 duration-300 hover:bg-neutral-600">
+          <Home />
+        </div>
+        <div className="bg-white rounded-full p-2 duration-300 hover:bg-neutral-600">
+          <Heart />
+        </div>
+        <div className="bg-white rounded-full p-2 duration-300 hover:bg-neutral-600">
+          <ShoppingBasket />
+        </div>
       </div>
       <div className="flex flex-col gap-5">
-        <User />
-        <Settings />
+        <div className="bg-white rounded-full p-2 duration-300 hover:bg-neutral-600">
+          <User />
+        </div>
+        <div className="bg-white rounded-full p-2 duration-300 hover:bg-neutral-600">
+          <Settings />
+        </div>
       </div>
     </section>
   );

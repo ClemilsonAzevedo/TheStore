dcb995130186abbad9d595ae86dee4b735c1a86e
PedroHenriqueMG <pedrohdefsouza@gmail.com>
2024-04-24 16:08:58 -0300

    feat: create aside menu

--- web/src/components/aside/aside.tsx
+++ web/src/components/aside/aside.tsx
@@ -1,7 +1,27 @@
+import {
+  ShoppingCart,
+  Home,
+  Heart,
+  ShoppingBasket,
+  User,
+  Settings,
+} from "lucide-react";
+
 export default function Aside() {
   return (
-    <section>
-      <p>aside</p>
+    <section className="flex h-[550px] w-[50px] flex-col items-center justify-between">
+      <div>
+        <ShoppingCart />
+      </div>
+      <div className="flex flex-col gap-5">
+        <Home />
+        <Heart />
+        <ShoppingBasket />
+      </div>
+      <div className="flex flex-col gap-5">
+        <User />
+        <Settings />
+      </div>
     </section>
   );
 }

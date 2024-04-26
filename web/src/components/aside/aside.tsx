6854b7221267ffaf9175f742d2ade928304f2848
Clemilson Azevedo <Clemilsondeazevedo@gmail.com>
2024-04-26 15:33:26 +0100

    Feat: create link redirect to correct pages.

--- web/src/components/aside/aside.tsx
+++ web/src/components/aside/aside.tsx
@@ -12,19 +12,27 @@ export default function Aside() {
 		<aside className='flex h-full w-[50px] flex-col items-center justify-between py-5'>
 			<div>
 				<div className='bg-white relative rounded-full p-2 duration-300 hover:bg-neutral-400'>
-					<div className='absolute right-1 top-2 h-[5px] w-[5px] rounded-full bg-violet-500' />
-					<ShoppingCart />
+					<span className='absolute right-1 top-2 h-[5px] w-[5px] rounded-full bg-violet-500' />
+					<a href="/cart">
+						<ShoppingCart />
+					</a>
 				</div>
 			</div>
 			<div className='flex flex-col gap-5'>
 				<div className='bg-white rounded-full p-2 duration-300 hover:bg-neutral-400'>
-					<Home />
+					<a href="/">
+						<Home />
+					</a>
 				</div>
 				<div className='bg-white rounded-full p-2 duration-300 hover:bg-neutral-400'>
-					<Heart />
+					<a href="/loved">
+						<Heart />
+					</a>
 				</div>
 				<div className='bg-white rounded-full p-2 duration-300 hover:bg-neutral-400'>
-					<ShoppingBasket />
+					<a href="/products">
+						<ShoppingBasket />
+					</a>
 				</div>
 			</div>
 			<div className='flex flex-col gap-5'>

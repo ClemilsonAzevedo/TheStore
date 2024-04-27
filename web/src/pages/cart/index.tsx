66258daf4bc6cd1b68705141c1cf594dba59a751
Clemilson Azevedo <Clemilsondeazevedo@gmail.com>
2024-04-27 13:33:50 +0100

    Style: Continue cart page styles

--- web/src/pages/cart/index.tsx
+++ web/src/pages/cart/index.tsx
@@ -42,10 +42,33 @@ const invoices = [
 export function Cart() {
 	return (
 		<section className='w-full flex gap-5 p-5'>
-			<div className='w-full border'>
-				<div>clema</div>
-				<div>clema</div>
-				<div>clema</div>
+			<div className='w-full flex gap-5 h-[150px]'>
+				<div className='border border-neutral-400 rounded-lg min-w-[350px] h-full flex flex-col p-5 gap-2'>
+					<h3 className='font-medium text-xl'>Your Cart</h3>
+					<p className='text-sm '>
+						This page apresent all your orders for create new order click on
+						button in this card
+					</p>
+					<a
+						href='/products'
+						className='py-2 w-[106px] text-[10px] bg-violet-500 flex items-center justify-center text-neutral-50 rounded-lg ml-auto hover:bg-violet-500/70 transition'
+					>
+						Add new product
+					</a>
+				</div>
+
+				<div className='border border-neutral-400 rounded-lg min-w-[200px] h-full p-5 flex flex-col justify-end items-center'>
+					<span className='text-4xl font-bold mx-auto'>{invoices.length}</span>
+					<p className='text-sm mt-8'>Products Quatity</p>
+				</div>
+
+				<div className='border p-5 border-neutral-400 rounded-lg w-full h-full flex flex-col justify-between items-start'>
+					<span className='text-xl font-medium'>Total</span>
+					<p className='text-sm flex items-center gap-3'>
+						All products are under
+						<span className='text-2xl font-semibold'>$15,329</span>
+					</p>
+				</div>
 			</div>
 
 			<div className='border min-w-[429px] h-full p-5 flex flex-col justify-between'>
@@ -59,7 +82,7 @@ export function Cart() {
 									Total items:
 								</TableHead>
 								<TableHead className='font-medium text-right text-neutral-950 text-base'>
-									17
+									{invoices.length}
 								</TableHead>
 							</TableRow>
 						</TableHeader>
@@ -79,7 +102,7 @@ export function Cart() {
 					</Table>
 				</div>
 
-				<Button className='bg-violet-500 w-36 mx-auto hover:bg-violet-500/70'>
+				<Button className='bg-violet-500 w-36 mx-auto hover:bg-violet-500/70 focus-visible:ring-0'>
 					Pay
 				</Button>
 			</div>

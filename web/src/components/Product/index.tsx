4431528b822c41c020d925d57028c587c5a82875
Clemilson Azevedo <Clemilsondeazevedo@gmail.com>
2024-05-02 19:03:36 +0100

    Style: Component Produc

--- web/src/components/Product/index.tsx
+++ web/src/components/Product/index.tsx
@@ -1,35 +1,62 @@
 import { Heart, Plus } from 'lucide-react'
 import pcImage from '../../assets/MacbookProM1Pro.png'
+import { useState } from 'react'
+import { Button } from '../ui/button'
+interface ProductProps {
+	id: number
+	name: string
+	description: string
+	price: number
+	image?: string
+}
 
-export function Product() {
+export function Product({
+	description,
+	name,
+	price,
+	image = pcImage,
+}: ProductProps) {
+	const [isProductLiked, setIsProductLiked] = useState(false)
+
+	function handleLikeProduct() {
+		setIsProductLiked(!isProductLiked)
+	}
+
 	return (
 		<div className='hover:bg-neutral-400 transition duration-300  rounded-lg px-5 hover:scale-110'>
 			<div className='flex flex-col gap-5 py-5 max-w-48 justify-center items-center'>
 				<div className='flex flex-col bg-neutral-400 w-max p-4 rounded-lg'>
-					<div className='flex items-center justify-center ml-auto -mr-2 -mt-2 bg-neutral-50 p-1 rounded-full cursor-pointer'>
+					<button
+						onClick={handleLikeProduct}
+						type='button'
+						className={
+							!isProductLiked
+								? 'flex items-center justify-center ml-auto -mr-2 -mt-2 bg-neutral-50 p-1 rounded-full cursor-pointer focus:outline-none focus:ring-0 ease-in-out hover:scale-110 transition'
+								: 'flex items-center justify-center ml-auto -mr-2 -mt-2 bg-violet-500 p-1 rounded-full cursor-pointer text-neutral-50 focus:outline-none focus:ring-0 repeat-1 duration-300 animate-in ease-in-out scale-125'
+						}
+					>
 						<Heart className='flex-1' size={16} />
-					</div>
-					<img src={pcImage} alt='Macbook Pro' />
+					</button>
+					<img src={image} alt='Macbook Pro' />
 				</div>
-				<div>
-					<h4 className='font-medium text-base text-neutral-950'>
-						Apple Macbook Pro
+				<div className='w-full'>
+					<h4 className='font-medium text-base text-neutral-950 text-center text-ellipsis truncate'>
+						{name}
 					</h4>{' '}
 					<p className='mt-4 text-[0.625rem] text-neutral-600 text-justify h-11 truncate text-wrap'>
-						Lorem ipsum dolor sit amet, consectetur adipiscing elit cleinsdjk
-						sklamnimdii sksnakjk dpkmidi didincn
+						{description}
 					</p>
 				</div>
 				<div className='flex w-full justify-between items-center'>
 					<span className='text-neutral-950 font-medium text-base'>
-						$ 1.838,69
+						$ {price}
 					</span>
-					<button
+					<Button
 						className='bg-violet-500 text-neutral-50 p-2 rounded-lg hover:bg-violet-500/70 transition'
 						type='button'
 					>
 						<Plus size={24} />
-					</button>
+					</Button>
 				</div>
 			</div>
 		</div>

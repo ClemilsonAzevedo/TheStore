0b9eb9603b04a1c8c23c24eba6b209b7c746a1a7
PedroHenriqueMG <pedrohdefsouza@gmail.com>
2024-05-03 21:39:41 -0300

    feat: add and test prisma and docker compose

---
+++ server/prisma/migrations/20240504003009_teste/migration.sql
@@ -0,0 +1,6 @@
+-- CreateTable
+CREATE TABLE "Teste" (
+    "id" TEXT NOT NULL,
+
+    CONSTRAINT "Teste_pkey" PRIMARY KEY ("id")
+);
--- server/src/server.ts
+++ server/src/server.ts
@@ -1,4 +1,5 @@
 import Fastify from "fastify";
+import prisma from "./client/client";
 
 const app = Fastify();
 
@@ -6,12 +7,22 @@ app.get("/", (req, res) => {
 	res.send("API rodando");
 });
 
+prisma
+	.$connect()
+	.then(() => {
+		console.log("Conexão com o banco de dados estabelecida com sucesso.");
+	})
+	.catch((error: Error) => {
+		throw new Error(error.message);
+	});
+
 const start = async () => {
 	try {
 		await app.listen({ port: 8080 });
 		app.log.info(`API rodando na porta ${8080}`);
-	} catch (error) {
-		app.log.error(error);
+	} catch (error: unknown) {
+		const newError = error as Error;
+		app.log.error(newError);
 		process.exit(1);
 	}
 };

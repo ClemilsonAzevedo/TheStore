51997776b87efcfb02708607f35c881a69615e77
Clemilson Azevedo <Clemilsondeazevedo@gmail.com>
2024-05-05 10:41:24 +0100

    Fix: Bild of app

--- web/src/utils/calculeQuantityAndAmountProducts.ts
+++ web/src/utils/calculeQuantityAndAmountProducts.ts
@@ -5,7 +5,11 @@ export let totalQuantity = 0
 export let totalAmountCartItems = 0
 
 for (const product of products) {
-	if (product.onCart) {
+	if (
+		product.onCart &&
+		product.cart?.quantity !== undefined &&
+		product.cart?.amount !== undefined
+	) {
 		totalQuantity += product.cart.quantity
 		totalAmountCartItems += product.cart.amount
 	}

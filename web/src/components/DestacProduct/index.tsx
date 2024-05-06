de6e889002775d1396078d6b7d004e1bbb615870
Clemilson Azevedo <Clemilsondeazevedo@gmail.com>
2024-05-06 02:35:34 +0100

    Styles: Liked button on Loved-page

--- web/src/components/DestacProduct/index.tsx
+++ web/src/components/DestacProduct/index.tsx
@@ -1,5 +1,6 @@
 import type { ProductInterface } from '@/@types/ProductInterface'
-import pcImage from '../../assets/MacbookProM1Pro.png'
+import pcImage from '@/assets/MacbookProM1Pro.png'
+import { Heart } from 'lucide-react'
 
 interface ProductProps extends ProductInterface {}
 
@@ -8,12 +9,23 @@ export function DestacProduct({ name, price, image = pcImage }: ProductProps) {
 		<div className='border-2 min-w-max rounded-lg p-5 transition hover:scale-110 hover:bg-neutral-400 cursor-pointer'>
 			<div className='flex flex-col gap-5 justify-center items-center'>
 				<div className='flex flex-col h-full'>
+					<button
+						type='button'
+						className={
+							'flex items-center justify-center ml-auto -mr-2 -mt-2 bg-violet-500 p-1 rounded-full cursor-pointer text-neutral-50 focus:outline-none focus:ring-0 repeat-1 duration-300 animate-in ease-in-out scale-125'
+						}>
+						<Heart className='flex-1' size={16} />
+					</button>
 					<img src={image} alt='' loading='lazy' className='w-80 h-auto  ' />
 				</div>
 				<h4 className='font-medium text-xl text-neutral-950 text-center text-ellipsis truncate'>
 					{name}
 				</h4>
-				<span className='text-neutral-950 font-semibold text-2xl'>{price}</span>
+				<div>
+					<span className='text-neutral-950 font-semibold text-2xl'>
+						{price}
+					</span>
+				</div>
 			</div>
 		</div>
 	)

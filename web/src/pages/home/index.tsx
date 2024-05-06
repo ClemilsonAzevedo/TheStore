f0c774c148d4a163e2c12302ff45d85e5d745119
Clemilson Azevedo <Clemilsondeazevedo@gmail.com>
2024-05-06 14:17:28 +0100

    Styles: Add section on Home Page

--- web/src/pages/home/index.tsx
+++ web/src/pages/home/index.tsx
@@ -12,12 +12,12 @@ import pcImage from '../../assets/MacbookProM1Pro.png'
 
 export function Home() {
 	return (
-		<section className='flex flex-col w-full gap-5 pr-5 overflow-y-auto'>
+		<section className='flex flex-col w-full space-y-10 pr-5 overflow-y-auto'>
 			<div className='flex flex-col justify-end items-center min-h-[550px] border-2 border-neutral-400 rounded-lg p-5'>
 				<div className='my-auto flex w-full'>
-					<div className='flex flex-wrap gap-5 max-w-[220px] h-[220px]'>
+					<div className='grid grid-cols-[100px_100px] flex-wrap gap-5 w-[340px] h-[220px]'>
 						<div className='w-[100px] h-[100px] bg-neutral-400 rounded-lg' />
-						<div className='w-[100px] h-[100px] bg-neutral-400 rounded-lg' />
+						<div className='w-[100px] h-[100px] bg-neutral-400 rounded-lg mr-auto' />
 						<div className='w-[100px] h-[100px] bg-neutral-400 rounded-lg' />
 					</div>
 
@@ -42,51 +42,28 @@ export function Home() {
 				</div>
 
 				<div className='flex justify-between items-center w-full'>
-					<span className='flex items-center gap-2 text-neutral-950 text-lg transition hover:text-violet-500/70'>
+					<a
+						href='#Smartwatches'
+						className='flex items-center gap-2 text-neutral-950 text-lg transition hover:text-violet-500/70'>
 						<Watch /> Smartwatches
-					</span>
-					<span className='flex items-center gap-2 text-neutral-950 text-lg transition hover:text-violet-500/70'>
+					</a>
+					<a
+						href='#Smartphones'
+						className='flex items-center gap-2 text-neutral-950 text-lg transition hover:text-violet-500/70'>
 						<Smartphone /> Smartphones
-					</span>
-					<span className='flex items-center gap-2 text-neutral-950 text-lg transition hover:text-violet-500/70'>
+					</a>
+					<a
+						href='#Headphones'
+						className='flex items-center gap-2 text-neutral-950 text-lg transition hover:text-violet-500/70'>
 						<Headphones />
 						Headphones
-					</span>
-					<span className='flex items-center gap-2 text-neutral-950 text-lg transition hover:text-violet-500/70'>
+					</a>
+					<a
+						href='#Laptops'
+						className='flex items-center gap-2 text-neutral-950 text-lg transition hover:text-violet-500/70'>
 						<Laptop /> Laptops
-					</span>
+					</a>
 				</div>
-			</div>
-
-			<div
-				id='Home'
-				className='flex items-start justify-center h-full flex-col px-5'>
-				<h3 className='font-semibold text-2xl mb-2'>Most Recent</h3>
-				<Carousel
-					opts={{
-						align: 'center',
-						loop: true,
-						slidesToScroll: 'auto',
-					}}
-					className='w-full max-w-screen-xl h-full  mx-auto'>
-					<CarouselContent>
-						{products.map(product => {
-							return (
-								<Product
-									key={product.id}
-									id={product.id}
-									price={product.price}
-									onCart={product.onCart}
-									isLiked={product.isLiked}
-									name={product.name}
-									description={product.description}
-								/>
-							)
-						})}
-					</CarouselContent>
-					<CarouselPrevious />
-					<CarouselNext />
-				</Carousel>
 			</div>
 
 			<div className='flex gap-5 justify-evenly'>
@@ -149,8 +126,10 @@ export function Home() {
 				</div>
 			</div>
 
-			<div className='flex items-start justify-center h-full flex-col px-5'>
-				<h3 className='font-semibold text-2xl mb-2'>Most Sells</h3>
+			<div
+				id='Home'
+				className='flex items-start justify-center h-full flex-col px-5'>
+				<h3 className='font-semibold text-2xl mb-2'>Most Recent</h3>
 				<Carousel
 					opts={{
 						align: 'center',
@@ -176,6 +155,96 @@ export function Home() {
 					<CarouselPrevious />
 					<CarouselNext />
 				</Carousel>
+			</div>
+
+			<div id='Smartwatches' className='flex flex-col items-center gap-5'>
+				<h2 className='flex gap-2 items-center text-2xl font-semibold sticky top-0 bg-neutral-50 w-full justify-center py-2'>
+					<Watch size={32} />
+					Smartwatches
+				</h2>
+
+				<div className='flex flex-wrap justify-center'>
+					{products.map(product => {
+						return (
+							<Product
+								key={product.id}
+								id={product.id}
+								price={product.price}
+								onCart={product.onCart}
+								isLiked={product.isLiked}
+								name={product.name}
+								description={product.description}
+							/>
+						)
+					})}
+				</div>
+			</div>
+
+			<div id='Smartphones' className='flex flex-col items-center gap-5'>
+				<h2 className='flex gap-2 items-center text-2xl font-semibold sticky top-0 bg-neutral-50 w-full justify-center py-2'>
+					<Smartphone size={32} /> Smartphones
+				</h2>
+
+				<div className='flex flex-wrap justify-center'>
+					{products.map(product => {
+						return (
+							<Product
+								key={product.id}
+								id={product.id}
+								price={product.price}
+								onCart={product.onCart}
+								isLiked={product.isLiked}
+								name={product.name}
+								description={product.description}
+							/>
+						)
+					})}
+				</div>
+			</div>
+
+			<div id='Headphones' className='flex flex-col items-center gap-5'>
+				<h2 className='flex gap-2 items-center text-2xl font-semibold sticky top-0 bg-neutral-50 w-full justify-center py-2'>
+					<Headphones size={32} />
+					Headphones
+				</h2>
+
+				<div className='flex flex-wrap justify-center'>
+					{products.map(product => {
+						return (
+							<Product
+								key={product.id}
+								id={product.id}
+								price={product.price}
+								onCart={product.onCart}
+								isLiked={product.isLiked}
+								name={product.name}
+								description={product.description}
+							/>
+						)
+					})}
+				</div>
+			</div>
+
+			<div id='Laptops' className='flex flex-col items-center gap-5'>
+				<h2 className='flex gap-2 items-center text-2xl font-semibold sticky top-0 bg-neutral-50 w-full justify-center py-2'>
+					<Laptop size={32} />
+					Laptops
+				</h2>
+				<div className='flex flex-wrap justify-center'>
+					{products.map(product => {
+						return (
+							<Product
+								key={product.id}
+								id={product.id}
+								price={product.price}
+								onCart={product.onCart}
+								isLiked={product.isLiked}
+								name={product.name}
+								description={product.description}
+							/>
+						)
+					})}
+				</div>
 			</div>
 		</section>
 	)
--- web/src/routes.tsx
+++ web/src/routes.tsx
@@ -2,9 +2,7 @@ import { Route, Routes as RoutesDOM } from 'react-router-dom'
 import { LayoutDefault } from './layouts/layoutDefault'
 import { Cart } from './pages/cart'
 import { Home } from './pages/home'
-import { Login } from './pages/login'
 import { LovedProducts } from './pages/loved-products'
-import SignUp from './pages/sign-up'
 import { Store } from './pages/store'
 
 export default function Routes() {
@@ -15,8 +13,6 @@ export default function Routes() {
 				<Route path='/cart' element={<Cart />} />
 				<Route path='/loved' element={<LovedProducts />} />
 				<Route path='/store' element={<Store />} />
-				<Route path='/signup' element={<SignUp />} />
-				<Route path='/login' element={<Login />} />
 			</Route>
 		</RoutesDOM>
 	)

9682257b100018007d0e46b58b7659a4a4b41fc5
Clemilson Azevedo <Clemilsondeazevedo@gmail.com>
2024-05-17 09:11:33 +0100

    Feat: Add list of products on CMDK

--- web/src/components/Header/index.tsx
+++ web/src/components/Header/index.tsx
@@ -12,8 +12,19 @@ import {
 import { Search } from 'lucide-react'
 import { type KeyboardEvent, useEffect, useState } from 'react'
 
+interface typeProductProps {
+	type: 'laptop' | 'headphone' | 'smartphone' | 'smartwatch'
+}
+
 export function Header() {
 	const [open, setOpen] = useState(false)
+
+	const typeOfProcuts: typeProductProps[] = [
+		{ type: 'laptop' },
+		{ type: 'headphone' },
+		{ type: 'smartphone' },
+		{ type: 'smartwatch' },
+	]
 
 	useEffect(() => {
 		const down = (e: KeyboardEvent) => {
@@ -29,42 +40,42 @@ export function Header() {
 	return (
 		<>
 			<header className='flex items-center justify-center p-5'>
-				<div className='flex items-center h-full w-full max-w-[400px] border border-neutral-600 rounded-lg p-2 focus-within:ring-1 focus-within:ring-violet-500 '>
-					<Search />
-					<input
-						list='all-suggestions'
-						type='search'
-						className=' flex-1 focus-visible:outline-none'
+				<button
+					type='button'
+					className='group flex items-center h-full w-full max-w-[350px] border-2 border-neutral-600 rounded-lg p-2 hover:border-violet-500 hover:bg-neutral-400/70 transition focus-within:ring-1 focus:border-violet-500 '
+					onClick={open ? () => setOpen(false) : () => setOpen(true)}>
+					<Search
+						size={20}
+						className='text-neutral-600 group-hover:text-neutral-950 transition'
 					/>
-					<kbd className='pointer-events-none inline-flex h-5 select-none items-center gap-1 rounded border bg-muted px-1.5 font-mono text-[10px] font-medium text-muted-foreground '>
+					<span className='flex-1 flex pl-2 text-neutral-600 '>
+						You will search here ...
+					</span>
+					<kbd className='pointer-events-none inline-flex select-none items-center rounded-lg border bg-neutral-400 group-hover:bg-violet-500 group-hover:text-neutral-50 transition px-1.5 py-px font-mono text-[10px] font-medium text-neutral-600 '>
 						<span className='text-sm bg-transparent'>Ctrl + K</span>
 					</kbd>
-
-					<datalist id='all-suggestions'>
-						{products.map(product => (
-							<option key={product.id} value={product.name}>
-								{product.name}
-							</option>
-						))}
-					</datalist>
-				</div>
+				</button>
 			</header>
 			<CommandDialog open={open} onOpenChange={setOpen}>
-				<Command className='flex items-center w-[400px] rounded-full bg-neutral-400 focus-within:ring-1 focus-within:ring-violet-500 focus-within:fill-neutral-950'>
+				<Command className='flex items-center w-[400px] rounded-full bg-neutral-400 focus-within:ring-1 focus-within:ring-violet-500'>
 					<CommandInput
 						id='search'
 						list='search-suggestions'
 						className='bg-transparent border-0 outline-none h-7 shadow-none focus:border-0 placeholder:text-neutral-600 flex-1'
-						placeholder='You will search here ...'
+						placeholder='Type for filter an product'
 					/>
 				</Command>
 
 				<CommandList>
 					<CommandEmpty>No results found.</CommandEmpty>
-					{products.map(product => (
-						<CommandItem key={product.id} value={product.name}>
-							{product.name}
-						</CommandItem>
+					{typeOfProcuts.map(product => (
+						<CommandGroup key={product.type} heading={product.type}>
+							{products
+								.filter(item => item.type === product.type)
+								.map(item => (
+									<CommandItem key={item.id}>{item.name}</CommandItem>
+								))}
+						</CommandGroup>
 					))}
 				</CommandList>
 			</CommandDialog>

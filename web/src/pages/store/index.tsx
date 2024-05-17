c7509085302c89a239677d99a830674582192810
Clemilson Azevedo <Clemilsondeazevedo@gmail.com>
2024-05-17 15:54:50 +0100

    Feat: Filter on Store Page

--- web/src/pages/store/index.tsx
+++ web/src/pages/store/index.tsx
@@ -1,5 +1,6 @@
 import { products } from '@/api/fakeProductsProps'
 import { Product } from '@/components/Product'
+import { Input } from '@/components/ui/input'
 import {
 	Select,
 	SelectContent,
@@ -9,30 +10,54 @@ import {
 	SelectTrigger,
 	SelectValue,
 } from '@/components/ui/select'
+import { Filter } from 'lucide-react'
+import { useState } from 'react'
 
 export function Store() {
-	return (
-		<section className='flex flex-wrap gap-5 justify-center w-full items-start py-5 overflow-y-auto'>
-			<div className='flex flex-col gap-5 items-center mx-5'>
-				<div className='mr-auto'>
-					<Select>
-						<SelectTrigger className='w-[180px] focus-visible:ring-violet-500	'>
-							<SelectValue placeholder='All Products' />
-						</SelectTrigger>
-						<SelectContent>
-							<SelectGroup>
-								<SelectLabel>Category</SelectLabel>
-								<SelectItem value='laptops'>Laptops</SelectItem>
-								<SelectItem value='Smartphones'>Smartphones</SelectItem>
-								<SelectItem value='headphones'>Headphones</SelectItem>
-								<SelectItem value='smartwatches'>Smartwatches</SelectItem>
-							</SelectGroup>
-						</SelectContent>
-					</Select>
-				</div>
+	const [allProducts, _] = useState(products)
+	const [filterOnPage, setFilterOnPage] = useState(products)
 
+	return (
+		<section className='flex gap-5 justify-start flex-col w-full py-5 overflow-y-auto'>
+			<div className='flex w-full items-center gap-5 flex-1'>
+				<div className='flex items-center gap-2 border w-[350px] py-2 pl-5 rounded-lg focus-within:border-violet-500'>
+					<label htmlFor='filter'>
+						<Filter size={24} />
+					</label>
+					<Input
+						id='filter'
+						placeholder='Filter name...'
+						className='border-none shadow-none outline-none placeholder:text-sm placeholder:text-neutral-600 text-sm'
+						onChange={e => {
+							const { value } = e.target
+							setFilterOnPage(
+								allProducts.filter(filteredProduct =>
+									filteredProduct.name
+										.toLowerCase()
+										.includes(value.toLowerCase()),
+								),
+							)
+						}}
+					/>
+				</div>
+				<Select>
+					<SelectTrigger className='w-[180px] focus-visible:ring-violet-500 h-full'>
+						<SelectValue placeholder='All Products' />
+					</SelectTrigger>
+					<SelectContent>
+						<SelectGroup>
+							<SelectLabel>Category</SelectLabel>
+							<SelectItem value='laptops'>Laptops</SelectItem>
+							<SelectItem value='Smartphones'>Smartphones</SelectItem>
+							<SelectItem value='headphones'>Headphones</SelectItem>
+							<SelectItem value='smartwatches'>Smartwatches</SelectItem>
+						</SelectGroup>
+					</SelectContent>
+				</Select>
+			</div>
+			<div className='flex flex-col gap-5 items-center h-full mx-5'>
 				<div className='flex flex-wrap gap-5 justify-center w-full mb-5'>
-					{products.map((product, _) => (
+					{filterOnPage.map((product, _) => (
 						<Product
 							key={product.id}
 							id={product.id}

92ccabcde6bfaf54e2e15e7879bd4f575eed34e0
PedroHenriqueMG <pedrohdefsouza@gmail.com>
2024-05-28 19:13:42 -0300

    add: add swagger on project

--- server/src/routes/router.ts
+++ server/src/routes/router.ts
@@ -1,6 +1,12 @@
 import { app } from "../server";
+import swagger from "@fastify/swagger";
+import swaggerui from "@fastify/swagger-ui";
 import { userRoutes } from "./userRoutes/userRoute";
 
 export async function routes() {
+	app.register(swagger);
+	app.register(swaggerui, {
+		routePrefix: "/docs",
+	});
 	app.register(userRoutes);
 }

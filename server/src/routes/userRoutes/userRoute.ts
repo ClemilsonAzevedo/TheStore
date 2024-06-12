1f96b9a13941a7f4594e59984b9fadb2fe8e69ea
PedroHenriqueMG <pedrohdefsouza@gmail.com>
2024-06-12 12:07:35 -0300

    fix: user controller

--- server/src/routes/userRoutes/userRoute.ts
+++ server/src/routes/userRoutes/userRoute.ts
@@ -1,30 +1,29 @@
 import { $ref } from "../../lib/schema";
-import { signUpController } from "../../controllers/signUpController";
 import { app } from "../../server";
-import { UserController } from "../../controllers/signInController";
+import { UserController } from "../../controllers/userController";
 
 export async function userRoutes() {
-	app.post(
-		"/signup",
-		{
-			schema: {
-				tags: ["User"],
-				body: $ref("signUpSchema"),
-				response: {
-					200: $ref("signUpResponse"),
-				},
-			},
-		},
-		new UserController().signUp,
-	);
-	app.post(
-		"/signin",
-		{
-			schema: {
-				tags: ["User"],
-				body: $ref("signInSchema"),
-			},
-		},
-		new UserController().signIn,
-	);
+  app.post(
+    "/user/signup",
+    {
+      schema: {
+        tags: ["User"],
+        body: $ref("signUpSchema"),
+        response: {
+          200: $ref("signUpResponse"),
+        },
+      },
+    },
+    new UserController().signUp
+  );
+  app.post(
+    "/user/signin",
+    {
+      schema: {
+        tags: ["User"],
+        body: $ref("signInSchema"),
+      },
+    },
+    new UserController().signIn
+  );
 }

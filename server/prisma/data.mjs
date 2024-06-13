80ad48d2ca7424992412b9bba9cbaafeea224045
PedroHenriqueMG <pedrohdefsouza@gmail.com>
2024-06-13 10:28:16 -0300

    add: create seed products on db

---
+++ server/prisma/data.mjs
@@ -0,0 +1,50 @@
+export const products = [
+  {
+    name: "Laptop",
+    description: "melhor produto",
+    price: 1000.0,
+    amount: 0,
+  },
+  {
+    name: "Smartphone",
+    description: "melhor produto",
+    price: 500.0,
+    amount: 10,
+  },
+  {
+    name: "Headphones",
+    description: "melhor produto",
+    price: 100.0,
+    amount: 20,
+  },
+  {
+    name: "Keyboard",
+    description: "melhor produto",
+    price: 50.0,
+    amount: 30,
+  },
+  {
+    name: "Mouse",
+    description: "melhor produto",
+    price: 20.0,
+    amount: 40,
+  },
+  {
+    name: "Monitor",
+    description: "melhor produto",
+    price: 300.0,
+    amount: 15,
+  },
+  {
+    name: "Printer",
+    description: "melhor produto",
+    price: 200.0,
+    amount: 12,
+  },
+  {
+    name: "Camera",
+    description: "melhor produto",
+    price: 400.0,
+    amount: 8,
+  },
+];

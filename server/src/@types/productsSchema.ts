1932a4f1dcc0ab96309b6eb29e250a42760cb3da
PedroHenriqueMG <pedrohdefsouza@gmail.com>
2024-06-14 22:58:05 -0300

    feat: create user and liked data

--- server/src/@types/productsSchema.ts
+++ server/src/@types/productsSchema.ts
@@ -1,13 +1,21 @@
 import { z } from "zod";
 
 export const productsSchema = z.object({
-	id: z.number().int(),
-	name: z.string(),
-	description: z.string(),
-	amount: z.number().int(),
-	price: z.number(),
+  id: z.number().int(),
+  name: z.string(),
+  description: z.string(),
+  amount: z.number().int(),
+  price: z.number(),
+});
+
+export const likedSchema = z.object({
+  id: z.number().int(),
+  products_ids: z.number().int(),
+  user_id: z.string(),
 });
 
 export const getAllResponse = z.array(productsSchema);
 
 export type ProductsProps = z.infer<typeof productsSchema>;
+
+export type LikedProps = z.infer<typeof likedSchema>;
--- server/src/controllers/productsController.ts
+++ server/src/controllers/productsController.ts
@@ -1,31 +1,37 @@
 import { FastifyReply, FastifyRequest } from "fastify";
 import { db } from "../db/db";
-import { ProductsProps } from "../@types/productsSchema";
+import { LikedProps, ProductsProps } from "../@types/productsSchema";
 
 export class ProductsController {
-	async getAll(req: FastifyRequest, res: FastifyReply) {
-		const products = await db.products.findMany();
+  async getAll(req: FastifyRequest, res: FastifyReply) {
+    const products = await db.products.findMany();
 
-		return res.send(products);
-	}
-	async getOne(
-		req: FastifyRequest<{
-			Params: ProductsProps;
-		}>,
-		res: FastifyReply,
-	) {
-		const { id } = req.params;
+    return res.send(products);
+  }
+  async getOne(
+    req: FastifyRequest<{
+      Params: ProductsProps;
+    }>,
+    res: FastifyReply
+  ) {
+    const { id } = req.params;
 
-		const product = await db.products.findUnique({
-			where: {
-				id: id,
-			},
-		});
+    const product = await db.products.findUnique({
+      where: {
+        id: id,
+      },
+    });
 
-		if (!product) {
-			return res.code(404).send({ message: "Produto não encontrado." });
-		}
+    if (!product) {
+      return res.code(404).send({ message: "Produto não encontrado." });
+    }
 
-		return res.send(product);
-	}
+    return res.send(product);
+  }
+  async createLiked(
+    req: FastifyRequest<{ Body: LikedProps }>,
+    res: FastifyReply
+  ) {
+    const { products_ids } = req.body;
+  }
 }
